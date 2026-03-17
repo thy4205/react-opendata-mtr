99d66a3f9d98f83e7c52c9c4a3ac46d577dd0f83
thy4205 <thy4205@gmail.com>
2026-03-17 16:04:55 +0800

    intergrate with material ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@ import Papa from 'papaparse';  // Removed unused imports for simplicity
 import "./App.css";
 import axios from 'axios';
 import routeDataFilePath from '/src/routes.json';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   const [upt, setUpdTime] = useState(0);
   const [lineData, setLineData] = useState({});  // Changed to string for line codes
   const [lineValue, setLineValue] = useState("");  // Changed to string for line codes
   const [staValue, setStaValue] = useState("");
-  const [routeColour, setRouteColour] = useState("");
+  const [routeColour, setRouteColour] = useState("#0b089e");
   const [fullList, setFullList] = useState({});  // Moved to state
   const [loading, setLoading] = useState(true);  // For loading state
   const [error, setError] = useState(null);
@@ -78,7 +92,7 @@ function App() {
   const handleLineChange = async (event) => {
     setLineValue(event.target.value);
     console.log(fullList[event.target.value]);
-
+    setRouteColour(routeDataFilePath['routes'][event.target.value]['colour']);
 
     // You can add logic here to populate stations based on selected line
   };
@@ -115,10 +129,10 @@ function App() {
     }
     if (lineValue && staValue) {
       console.log(fullList[lineValue][arrivalTime[direction][seq]['dest']]['cname']);
-      var destFullName = fullList[lineValue][arrivalTime[direction][seq]['dest']]['cname'] + fullList[lineValue][arrivalTime[direction][seq]['dest']]['ename']
+      var destFullName = [fullList[lineValue][arrivalTime[direction][seq]['dest']]['cname'], fullList[lineValue][arrivalTime[direction][seq]['dest']]['ename']]
       //console.log(fullList[lineValue][arrivalTime[direction][seq]['dest']]['cname']);
     }
-    return <tr><td>{seq}</td><td>{arrivalTime[direction][seq]['plat'] !== 'undefined' ? arrivalTime[direction][seq]['plat'] : ""}</td><td>{arrivalTime[direction][seq]['dest'] !== 'undefined' ? destFullName : ""}</td><td>{arrivalTime[direction][seq]['ttnt'] ? (arrivalTime[direction][seq]['ttnt'] == 0 ? "即將到達" : arrivalTime[direction][seq]['ttnt'] + "分鐘") : ""}</td></tr>
+    return <tr><td>{seq}</td><td><div className="platformNumber" style={{ "backgroundColor": routeColour, color: 'white', width: "40px","border-radius":"20px" }}>{arrivalTime[direction][seq]['plat'] !== 'undefined' ? arrivalTime[direction][seq]['plat'] : ""}</div></td><td><Box component="span" sx={{ display: 'block', textAlign: 'left' }}>{arrivalTime[direction][seq]['dest'] !== 'undefined' ? destFullName[0] : ""}</Box><Box component="span" sx={{ display: 'block', textAlign: 'left' }}>{arrivalTime[direction][seq]['dest'] !== 'undefined' ? destFullName[1] : ""}</Box></td><td>{arrivalTime[direction][seq]['ttnt'] ? (arrivalTime[direction][seq]['ttnt'] == 0 ? "即將到達" : arrivalTime[direction][seq]['ttnt'] + "分鐘") : ""}</td></tr>
   }
 
   if (loading) return <p>Loading data...</p>;
@@ -126,59 +140,95 @@ function App() {
 
   return (
     <>
-      <h1>資訊顯示</h1>
-      <h3>上行</h3>
-      <table style={{ width: "100%" }}>
-        <tbody>
+      <Container maxWidth="sm">
+        <h1>資訊顯示</h1>
 
-
-
-          <Showttnt data={arrivalTime} seq="0" direction="UP" />
-          <Showttnt data={arrivalTime} seq="1" direction="UP" />
-          <Showttnt data={arrivalTime} seq="2" direction="UP" />
-          <Showttnt data={arrivalTime} seq="3" direction="UP" />
-        </tbody>
-      </table>
-      <h3>下行</h3>
-      <table style={{ width: "100%" }}>
-        <tbody>
-
-          <Showttnt data={arrivalTime} seq="0" direction="DOWN" />
-          <Showttnt data={arrivalTime} seq="1" direction="DOWN" />
-          <Showttnt data={arrivalTime} seq="2" direction="DOWN" />
-          <Showttnt data={arrivalTime} seq="3" direction="DOWN" />
-
-
-        </tbody>
-      </table>
-     
-      <div className="card">
         <form>
-          <select id="line" value={lineValue} onChange={handleLineChange}>
+          {/*  <select id="line" value={lineValue} >
             <option value="">Select a line</option>
             {(routeDataFilePath['routes']).map((line) => (
               <option key={line['short']} value={line['short']}>
                 {line['nameChi']}
               </option>
             ))}
-          </select>
-
-          <select value={staValue} onChange={lineChange}>
-            <option value="">Select a station</option>
-            {lineValue && fullList[lineValue] ? (
-              Object.entries(fullList[lineValue]).map(([code, stationData]) => (
-                <option key={code} value={code}>
-                  {stationData.cname}  {/* Shows English name; change to stationData.cname for Chinese */}
-                </option>
-              ))
-            ) : (
-              <option disabled>No stations available</option>
-            )}
-          </select>
-
-          <input type="submit" />
+          </select> */}
+          <Grid container spacing={2}>
+            <Grid size={5}>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="lineSelectLabel">Route</InputLabel>
+                  <Select
+                    labelId="lineSelectLabel"
+                    id="demo-simple-select"
+                    label="Age"
+                    onChange={handleLineChange}
+                    variant="filled"
+                  >
+                    {Object.entries(routeDataFilePath['routes']).map(([linekey, lineValue]) => (
+                      <MenuItem key={linekey} value={linekey}>
+                        {lineValue['nameChi']}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Box>
+            </Grid>
+            <Grid size={5}>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="stationSelectorLabel">Station</InputLabel>
+                  <Select
+                    labelId="stationSelectorLabel"
+                    id="stationSelector"
+                    label="Age"
+                    onChange={lineChange}
+                    variant="filled"
+                  >
+                    <MenuItem value="">Select a station</MenuItem>
+                    {lineValue && fullList[lineValue] ? (
+                      Object.entries(fullList[lineValue]).map(([code, stationData]) => (
+                        <MenuItem key={code} value={code}>
+                          {stationData.cname}  {/* Shows English name; change to stationData.cname for Chinese */}
+                        </MenuItem>
+                      ))
+                    ) : (
+                      <MenuItem disabled>No stations available</MenuItem>)}
+                  </Select>
+                </FormControl>
+              </Box>
+            </Grid>
+            <Grid size={2}>
+              <button type="submit" >Submit</button>
+            </Grid>
+          </Grid>
         </form>
-      </div>
+
+        <h3>上行</h3>
+        <table style={{ width: "100%" }}>
+          <tbody>
+
+
+
+            <Showttnt data={arrivalTime} seq="0" direction="UP" />
+            <Showttnt data={arrivalTime} seq="1" direction="UP" />
+            <Showttnt data={arrivalTime} seq="2" direction="UP" />
+            <Showttnt data={arrivalTime} seq="3" direction="UP" />
+          </tbody>
+        </table>
+        <h3>下行</h3>
+        <table style={{ width: "100%" }}>
+          <tbody>
+
+            <Showttnt data={arrivalTime} seq="0" direction="DOWN" />
+            <Showttnt data={arrivalTime} seq="1" direction="DOWN" />
+            <Showttnt data={arrivalTime} seq="2" direction="DOWN" />
+            <Showttnt data={arrivalTime} seq="3" direction="DOWN" />
+
+
+          </tbody>
+        </table>
+
+      </Container>
 
     </>
   );
